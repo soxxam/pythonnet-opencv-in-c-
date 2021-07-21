dad2ab4e4a2790b3bbb89922fc47fd7709a0fa8f
tu tran <sieutuhang1@outlook.com>
2021-07-21 15:28:01 +0700

    xong camera

--- opencv_py/formchucnang.cs
+++ opencv_py/formchucnang.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Python.Runtime;
+
 
 
 namespace opencv_py
@@ -51,7 +53,34 @@ namespace opencv_py
 
         private void button1_Click(object sender, EventArgs e)
         {
+            using (Py.GIL())
+            {
+                dynamic np = Py.Import("numpy");
+                dynamic cv2 = Py.Import("cv2");
+                dynamic cap1 = cv2.VideoCapture(0);
 
+
+
+                while (true)
+                {
+                    dynamic items1 = cap1.read();
+                    dynamic frame1 = items1[1];
+                    dynamic gray3 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY);
+                    gray3 = cv2.resize(gray3, null, fx: 1.0 / 2, fy: 1.0 / 2);
+                    cv2.imshow("hoge1", frame1);
+                    cv2.imshow("hoge3", gray3);
+
+
+                    int item = cv2.waitKey(20);
+                    if (item == 113)
+                    {
+                        cv2.destroyAllWindows(  );
+                        break;
+                    }
+
+                }
+                Console.ReadKey();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
